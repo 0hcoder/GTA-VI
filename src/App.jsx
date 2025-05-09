11c03188c1eb4a43e0d44702d0e1df78f3b55e5e
0hrawat <dev.ankurawat@gmail.com>
2025-05-10 01:39:23 +0530

    Adding landing page

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,29 @@ import gsap from "gsap";
 
 const App = () => {
   const [isVisible, setIsVisible] = useState(false);
-  useGSAP(()=>{
+  useGSAP(() => {
     const tl = gsap.timeline();
     tl.to("#vi-mask-group", {
-    rotate: 10,
-    ease: "power4.inOut",
-    transformOrigin: "50% 50%",
-    duration: 2,
-    })
-    .to("#vi-mask-group", {
-     scale: 10,
-     duration: 2,
-     delay: -1.8,
-     ease: "expo.easeInOut",
+      rotate: 10,
+      ease: "Power4.easeInOut",
+      transformOrigin: "50% 50%",
+      duration: 2,
+    }).to("#vi-mask-group", {
+      scale: 10,
+      duration: 2,
+      delay: -1.8,
+      ease: "Expo.easeInOut",
       transformOrigin: "50% 50%",
       opacity: 0,
-    })
-  })
+      onUpdate: function () {
+        if (this.progress() >= 0.9) {
+          document.querySelector(".svg").remove();
+          setIsVisible(true);
+          this.kill();
+        }
+      },
+    });
+  });
   return (
     <>
       <div className="svg flex ite justify-center fixed top-0 left-0  z-[10] w-full h-screen overflow-hidden bg-[#000] ">
@@ -32,7 +38,7 @@ const App = () => {
                 <text
                   x="50%"
                   y="50%"
-                  fontSize="300"
+                  fontSize="250"
                   textAnchor="middle"
                   fill="white"
                   dominantBaseline="middle"
@@ -55,6 +61,39 @@ const App = () => {
           />
         </svg>
       </div>
+      {isVisible && (
+        <div className="main w-full">
+          <div className="landing w-full h-screen bg-black">
+            <div className="navbar absolute top-0 left-0 z-[10] w-full py-10 px-10  text-white">
+              <div className="logo flex gap-6 items-center">
+                <div className="lines flex flex-col gap-[6px]">
+                  <div className="line w-14 h-1.5 bg-white"></div>
+                  <div className="line w-9 h-1.5 bg-white"></div>
+                  <div className="line w-7 h-1.5 bg-white"></div>
+                </div>
+                <h3 className="text-3xl -mt-[19px] leading-none">RockStar</h3>
+              </div>
+            </div>
+            <div className="imagesdiv relative w-full h-screen overflow-y-hidden">
+              <img
+                className="w-full h-full bg-cover absolute top-0 left-0"
+                src="./sky.png"
+                alt=""
+              />
+              <img
+                className="w-full h-full bg-cover absolute top-0 left-0"
+                src="./bg.png"
+                alt=""
+              />
+              <img
+                className="absolute -bottom-[70%] left-1/2 -translate-x-1/2 scale-[0.8]"
+                src="./girlbg.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
