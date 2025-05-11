d031e4f956b93b5fa4f66c6dbb1c6a19aef5ec0d
0hrawat <dev.ankurawat@gmail.com>
2025-05-11 12:07:06 +0530

    landing page done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import "remixicon/fonts/remixicon.css";
 
 const App = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -27,6 +28,22 @@ const App = () => {
       },
     });
   });
+  useGSAP(() => {
+    const main = document.querySelector(".main");
+    main?.addEventListener("mousemove", (e) => {
+      const xMove = (e.clientX / window.innerWidth - 0.5) * 40;
+      gsap.to(".imagesdiv .text", {
+        x: `${xMove * 0.4}%`,
+      });
+      gsap.to(".sky", {
+        x: xMove,
+      });
+
+      gsap.to(".bg", {
+        x: xMove * 1.7,
+      });
+    });
+  }, [isVisible]);
   return (
     <>
       <div className="svg flex ite justify-center fixed top-0 left-0  z-[10] w-full h-screen overflow-hidden bg-[#000] ">
@@ -62,8 +79,8 @@ const App = () => {
         </svg>
       </div>
       {isVisible && (
-        <div className="main w-full">
-          <div className="landing w-full h-screen bg-black">
+        <div className="main w-full Overflow-hidden">
+          <div className="landing w-full h-screen no-scrollbar bg-black">
             <div className="navbar absolute top-0 left-0 z-[10] w-full py-10 px-10  text-white">
               <div className="logo flex gap-6 items-center">
                 <div className="lines flex flex-col gap-[6px]">
@@ -76,21 +93,40 @@ const App = () => {
             </div>
             <div className="imagesdiv relative w-full h-screen overflow-y-hidden">
               <img
-                className="w-full h-full bg-cover absolute top-0 left-0"
+                className="sky w-full h-full bg-cover absolute scale-[1.2] top-0 left-0"
                 src="./sky.png"
                 alt=""
               />
               <img
-                className="w-full h-full bg-cover absolute top-0 left-0"
+                className="w-full bg scale-[1.2] h-full bg-cover absolute top-0 left-0"
                 src="./bg.png"
                 alt=""
               />
+              <div className="text absolute top-1.5 left-1/2 -translate-x-1/2 text-white flex flex-col gap-1">
+                <h1 className="text-[6.2rem] leading-none  -ml-16">grand</h1>
+                <h1 className="text-[6.2rem] leading-none  ml-14">theft</h1>
+                <h1 className="text-[6.2rem] leading-none  -ml-16">auto</h1>
+              </div>
               <img
                 className="absolute -bottom-[70%] left-1/2 -translate-x-1/2 scale-[0.8]"
                 src="./girlbg.png"
                 alt=""
               />
             </div>
+            <div className="btmbar w-full px-6 py-8 bg-gradient-to-t from-black to-transparent absolute bottom-0 left-0 z-[10]">
+              <div className="flex gap-4 items-center text-white ">
+                <i className="ri-arrow-down-line"></i>
+                <h3 className="font-[Helvetica_Now_Display]">Scrol Down</h3>
+              </div>
+              <img
+                className="h-[40px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                src="./ps5.png"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="w-full h-screen bg-amber-500">
+            <img className="w-full h-full bg-cover" src="https://images.alphacoders.com/135/thumb-1920-1355218.jpg" alt="" />
           </div>
         </div>
       )}
